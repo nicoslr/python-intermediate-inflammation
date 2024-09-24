da89a85076723f0662eef36981fca99bbe4cfbe1
nsoler <n.soler.alvarez.m@gmail.com>
2024-09-24 12:46:38 +0200

    Added Pylint library and style fixes

--- inflammation/models.py
+++ inflammation/models.py
@@ -17,32 +17,29 @@ def load_csv(filename):
     """
     return np.loadtxt(fname=filename, delimiter=',')
 
-
 def daily_mean(data):
     """Calculate the daily mean of a 2D inflammation data array.
 
-    :param data: A 2D data array with inflammation data (one row
-                 represents one patient and one col one day)
+    :param data: A 2D data array with inflammation data
+    (one row represents one patient and one col one day)
     :returns: An array of mean values of measurements for each day
     """
     return np.mean(data, axis=0)
 
-
 def daily_max(data):
     """Calculate the daily max of a 2D inflammation data array.
 
-    :param data: A 2D data array with inflammation data (one row
-                 represents one patient and one col one day)
+    :param data: A 2D data array with inflammation data
+    (one row represents one patient and one col one day)
     :returns: An array of max values of measurements for each day
     """
     return np.max(data, axis=0)
 
-
 def daily_min(data):
     """Calculate the daily min of a 2D inflammation data array.
 
-    :param data: A 2D data array with inflammation data (one row
-                 represents one patient and one col one day)
+    :param data: A 2D data array with inflammation data
+    (one row represents one patient and one col one day)
     :returns: An array of min values of measurements for each day
     """
     return np.min(data, axis=0)
