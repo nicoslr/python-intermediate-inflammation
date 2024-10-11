e2307239493537e08ccb968d401d515af658bd8e
nsoler <n.soler.alvarez.m@gmail.com>
2024-10-11 12:47:36 +0200

    Big catch-up

--- inflammation/compute_data.py
+++ inflammation/compute_data.py
@@ -35,21 +35,29 @@ class JSONDataSource:
     data = map(models.load_json, data_file_paths)
     return list(data)
 
+def compute_standard_deviation_by_day(data):
+    """
+    Calculates the standard deviation for a set of daily inflammation means
+    """
+    means_by_day = map(models.daily_mean, data)
+    means_by_day_matrix = np.stack(list(means_by_day))
+
+    daily_standard_deviation = np.std(means_by_day_matrix, axis=0)
+    return daily_standard_deviation
+
 def analyse_data(data_source):
     """Calculates the standard deviation by day between datasets.
 
     Gets all the inflammation data from CSV files within a directory,
     works out the mean inflammation value for each day across all datasets,
-    then plots the graphs of standard deviation of these means."""
+    and plots the standard deviation of these means."""
     data = data_source.load_inflammation_data()
 
-    means_by_day = map(models.daily_mean, data)
-    means_by_day_matrix = np.stack(list(means_by_day))
-
-    daily_standard_deviation = np.std(means_by_day_matrix, axis=0)
+    daily_standard_deviation = compute_standard_deviation_by_day(data)
 
     graph_data = {
         'standard deviation by day': daily_standard_deviation,
     }
-    views.visualize(graph_data)
+    # views.visualize(graph_data)
+    return daily_standard_deviation
 
--- inflammation/models.py
+++ inflammation/models.py
@@ -51,3 +51,19 @@ def daily_min(data):
     """Calculate the daily min of a 2d inflammation data array."""
     return np.min(data, axis=0)
 
+def patient_normalise(data):
+    """
+    Normalise patient data from a 2D inflammation data array.
+
+    NaN values are ignored, and normalised to 0.
+
+    Negative values are rounded to 0.
+    """
+    if np.any(data < 0):
+        raise ValueError('Inflammation values should not be negative')
+    max_data = np.nanmax(data, axis=1)
+    with np.errstate(invalid='ignore', divide='ignore'):
+        normalised = data / max[:, np.newaxis]
+    normalised[np.isnan(normalised)] = 0
+    normalised[normalised < 0] = 0
+    return normalised
