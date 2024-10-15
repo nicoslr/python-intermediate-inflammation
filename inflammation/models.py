Fixed detail of standard deviation function

inflammation/models.py
@@ -21,5 +21,5 @@ def daily_min(data):
     return np.min(data, axis=0)
 def standard_deviation(data):
     """Computes and returns standard deviation for data."""
-    standard_dev = np.std(data)
+    standard_dev = np.std(data, axis=0)
     return standard_dev